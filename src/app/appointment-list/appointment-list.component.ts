6e7c9cacccaed64350f6e2e548a98362c33817b0
Chris Allen <christopherallen777@gmail.com>
2024-01-30 22:22:40 -0600

    Add NgOnInit / local storage

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit{
 
   newAppointmentTitle: string = "";
   newAppointmentDate: Date = new Date();
-  appointments: Appointment[] = []
+
+  appointments: Appointment[] = [];
+
+  // Life cycle hook good for when data needs to be loaded when component gets created.
+  // - Import, Impliment, and then invoke
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem("appointments");
+
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [];
+  }
 
   addAppointment(){
     if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
